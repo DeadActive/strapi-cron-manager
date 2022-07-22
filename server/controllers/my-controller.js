4491314a52db55e449b7515ccc6c45cba8199b24
DeadActive <undead.active@gmail.com>
2022-07-22 13:32:24 +0300

    asdadsasd

--- server/controllers/my-controller.js
+++ server/controllers/my-controller.js
@@ -1,13 +1,17 @@
 "use strict";
 
-const pluginId = require("../../admin/src/pluginId");
-
 module.exports = {
     index(ctx) {
-        ctx.body = strapi.plugin(pluginId).service("myService").getCronList();
+        ctx.body = strapi
+            .plugin("cron-manager")
+            .service("myService")
+            .getCronList();
     },
     run(ctx) {
         const id = ctx.request.url.split("/")[3];
-        ctx.body = strapi.plugin(pluginId).service("myService").runTask(id);
+        ctx.body = strapi
+            .plugin("cron-manager")
+            .service("myService")
+            .runTask(id);
     },
 };
